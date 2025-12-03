2e5354ec462b51f0f5d5956ea5c0c0820aeb5443
Johan Lorck <kfj.lorck@orange.fr>
2025-12-04 00:55:00 +0100

    Change runtime to nodejs for secure env variables

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 /**
  * Vérifier la signature HMAC avec Web Crypto API (compatible edge runtime)
